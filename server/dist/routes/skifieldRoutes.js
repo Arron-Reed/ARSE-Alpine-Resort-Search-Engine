7d1e07af87ca50595c294a5199c0f658747fd037
Arron.Reed <arron.reed@chasacademy.se>
2023-11-11 02:33:16 +0100

    try again

--- server/dist/routes/skifieldRoutes.js
+++ server/dist/routes/skifieldRoutes.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -16,21 +7,21 @@ const express_1 = __importDefault(require("express"));
 const skifieldModel_1 = require("../models/skifieldModel");
 const router = express_1.default.Router();
 // Get ALL Skifields
-router.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+router.get("/", async (req, res) => {
     try {
-        const allSkifields = yield skifieldModel_1.SkifieldModel.find();
+        const allSkifields = await skifieldModel_1.SkifieldModel.find();
         res.status(200).json(allSkifields);
     }
     catch (error) {
         console.error(error);
         res.status(500).json({ error: "Internal server error" });
     }
-}));
+});
 // Search Bar for a skifield based on name
-router.get("/search", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+router.get("/search", async (req, res) => {
     try {
         const searchQuery = req.query.q;
-        const searchSkifields = yield skifieldModel_1.SkifieldModel.find({
+        const searchSkifields = await skifieldModel_1.SkifieldModel.find({
             name: { $regex: new RegExp(searchQuery, "i") }
         });
         res.status(200).json(searchSkifields);
@@ -39,12 +30,12 @@ router.get("/search", (req, res) => __awaiter(void 0, void 0, void 0, function* 
         console.error(error);
         res.status(500).json({ error: "Internal server error" });
     }
-}));
+});
 // Get One Skifield with :id in URL
-router.get("/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+router.get("/:id", async (req, res) => {
     const skifieldId = req.params.id;
     try {
-        const getSkifield = yield skifieldModel_1.SkifieldModel.findOne({ _id: skifieldId });
+        const getSkifield = await skifieldModel_1.SkifieldModel.findOne({ _id: skifieldId });
         if (!getSkifield) {
             return res.status(404).json({ error: "Skifield not found" });
         }
@@ -54,25 +45,25 @@ router.get("/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () 
         console.error(error);
         res.status(500).json({ error: "Server error" });
     }
-}));
+});
 // Create a skifield (not logged in)
-router.post("/create", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+router.post("/create", async (req, res) => {
     try {
         const skifield = req.body;
-        const newSkifield = yield skifieldModel_1.SkifieldModel.create(skifield);
+        const newSkifield = await skifieldModel_1.SkifieldModel.create(skifield);
         res.status(201).json(newSkifield);
     }
     catch (error) {
         console.error(error);
         res.status(500).json({ error: "Skifield creation failed" });
     }
-}));
+});
 // Update a skifield (not logged in)
-router.put("/update", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+router.put("/update", async (req, res) => {
     try {
         const skifieldId = req.body.id;
         const updatedSkifield = req.body;
-        const updatedRes = yield skifieldModel_1.SkifieldModel.findOneAndUpdate({ _id: skifieldId }, updatedSkifield, { new: true });
+        const updatedRes = await skifieldModel_1.SkifieldModel.findOneAndUpdate({ _id: skifieldId }, updatedSkifield, { new: true });
         if (!updatedRes) {
             return res.status(404).json({ error: "Skifield not found" });
         }
@@ -82,5 +73,5 @@ router.put("/update", (req, res) => __awaiter(void 0, void 0, void 0, function* 
         console.error(error);
         res.status(500).json({ error: "Skifield update failed" });
     }
-}));
+});
 exports.default = router;
